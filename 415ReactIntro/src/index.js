d695aaeae55fd583e6a635e2351a0b51d658f5e6
CarlosIUSalazar <carlosiusalazar@gmail.com>
2020-03-13 16:53:46 +0900

    Angela Yu ReactJS Course

---
+++ 415ReactIntro/src/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+//ReactDOM.render(What to show , where to show it , tell me when that render function has completed )
+ReactDOM.render(
+  <div>
+    <h1>Hello World!</h1>
+    <p>This is a paragraph.</p>
+  </div>,
+  document.getElementById("root")
+);
---
+++ 416ReactChallenge/src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1> My Favorite Cats</h1>
+    <ul>
+      <li> Gizmo </li>
+      <li> Minx </li>
+      <li> Cherry </li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
